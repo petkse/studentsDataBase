4abe4dd4c9f046cbf370406b8bac5b82c7712fb6
petkse <petkseart@gmail.com>
2026-03-02 04:37:38 +0300

    Обновила меню, добавив пункт 3

--- main.cpp
+++ main.cpp
@@ -59,6 +59,7 @@ int main() {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Удалить студента\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -69,6 +70,9 @@ int main() {
                 break;
             case 2:
                 displayStudents(database);
+                break;
+            case 3:
+                removeStudent(database);
                 break;
             case 0:
                 std::cout << "Выход из программы.\n";
